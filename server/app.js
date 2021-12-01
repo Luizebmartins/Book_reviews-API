6ca416847973fbb86f288dd9841c11d72f42aee2
Luiz Martins <luizmartins.prog@gmail.com>
2021-12-01 14:52:34 -0300

    add reviews routes

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ app.use('/', require('./routes/userRoutes'))
 app.use('/', require('./routes/bookRoutes'))
 app.use('/', require('./routes/reviewRoutes'))
 app.use('/', require('./routes/authorRoutes'))
+app.use('/', require('./routes/viewsRoutes'))
 app.use(require('./middlewares/errors'))
 
 app.listen(3000)
